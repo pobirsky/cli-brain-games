849a730fde02dbf5064fb8574787952ee7e5f36b
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-04-27 17:19:18 +0800

    7 steps brain-gcd.js

---
+++ src/games/brainGcd.js
@@ -0,0 +1,19 @@
+import getRandomInteger from '../rand.js';
+import playGame from '../index.js';
+
+function firstGcd(num1, num2) {
+  return (num2 === 0) ? num1 : firstGcd(num2, num1 % num2);
+}
+
+const directive = 'Find the greatest common divisor of given numbers.';
+
+const generateRound = () => {
+  const num1 = getRandomInteger(1, 50);
+  const num2 = getRandomInteger(1, 50);
+  const correctAnswer = firstGcd(num1, num2).toString();
+  const question = `${num1} ${num2}`;
+
+  return [question, correctAnswer];
+};
+
+export default () => playGame(directive, generateRound);
