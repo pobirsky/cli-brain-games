a014c6ba23d9e90e66c7f1d5d4ebe66343a40717
Danil Pobirskiy <danilpobiryan@yandex.ru>
2022-04-29 12:31:17 +0800

    fix brainPrime.js

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -4,15 +4,18 @@ import playGame from '../index.js';
 const directive = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num < 2) return false;
-  for (let i = 0; i < num; i += 1) {
-    if (num % i === 0) return false;
+  let flag = true;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      flag =  false;
+      break;
+    }
   }
-  return true;
+  return flag;
 };
 
 const generateRpound = () => {
-  const num = getRandomInteger(1, 1000);
+  const num = getRandomInteger(1, 10);
   const question = num;
   const answer = isPrime(num) ? 'yes' : 'no';
   return [question, answer];
